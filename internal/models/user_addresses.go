17db542dfec17900f740eb3b00f4e5f127cdb167
Manoj Kumar <kvssmk36@gmail.com>
2025-11-22 12:21:08 +0530

    fix: add json tags

--- internal/models/user_addresses.go
+++ internal/models/user_addresses.go
@@ -1,12 +1,12 @@
 package models
 
 type UserAddress struct {
-	Id             int
-	User_id        int
-	Label          string
-	Street_address string
-	City           string
-	State          string
-	Postal_code    string
-	Country        string
+	Id             int    `json:"id"`
+	User_id        int    `json:"user_id"`
+	Label          string `json:"label"`
+	Street_address string `json:"street_address"`
+	City           string `json:"city"`
+	State          string `json:"state"`
+	Postal_code    string `json:"postal_code"`
+	Country        string `json:"country"`
 }
